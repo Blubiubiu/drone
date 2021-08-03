34e0e23a72a49cecaf34c83e086dbc682a5302f2
billsyli <billsyli@tencent.com>
2021-08-03 10:37:28 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App123"/>
+    <HelloWorld msg="Welcome to Your Vue.js App1234"/>
   </div>
 </template>
 
