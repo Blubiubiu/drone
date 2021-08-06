0f52cbc44f3c763bc804195f313e8c514052509d
billsyli <billsyli@tencent.com>
2021-08-06 11:07:12 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App1"/>
+    HELLO WORLD!!!!
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
@@ -24,5 +19,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  font-size: 60px;
 }
 </style>
